7538a51d12828fed7fde5c9d1217f2e31f9c363a
DarlyneP <darlynep.devdoxs@gmail.com>
2023-12-12 21:45:44 +0100

    affichage des photographes sur la page d'accueil et redirection sur la page profil d'un photographe

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,9 @@
 //Mettre le code JavaScript lié à la page photographer.html
+const photographers = fetch("../../data/photographers.json")
+function getPhotographer() {
+    let photographerId = localStorage.id
+    const photographer = photographers.find( id => id = photographerId )
+    console.log(photographer);
+}
+
+getPhotographer()
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,16 +1,86 @@
+function loadUserProfile (event) {
+    let photographerId = event.currentTarget.id
+    console.log("id : ", photographerId)
+    localStorage.setItem("id", photographerId)
+
+    window.location.replace("photographer.html")
+}
+
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, tagline, price, id } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
+
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+
+        //& creating what goes inside of article
+        //* creating image wrapper for the profile picture (imgWrapper)
+        const imgWrapper = document.createElement('div')
+        imgWrapper.classList.add('profilepic--wrapper')
+
+        //? creating vignette
+        const vignette = document.createElement('a')
+        //vignette.setAttribute("href", "photographer.html") //! only with <a href=""> instead of "onclick"
+        vignette.setAttribute("id", id)
+        vignette.setAttribute("onclick", "loadUserProfile(event)")
+        vignette.classList.add('vignette');
+        //* creating name of the photographer
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        //* adding image slot then name in proper order according to html structure
+        article.appendChild(vignette);
+        vignette.appendChild(imgWrapper);
+        vignette.appendChild(h2);
+
+
+        //& creating what goes inside imgWrapper
+        //* creating the  background profile picture (profilepic pp--bg), the background cover (profilepic--bgcover), & the wrapper circle (imgWrapper wrapper--circle)
+        //^ pp--bg
+        const img = document.createElement( 'img' );
+        img.setAttribute("src", picture)
+        img.classList.add('profilepic')
+        img.classList.add('pp--bg')
+
+        //^ profilepic--bgcover
+        const bgCover = document.createElement( 'div' );
+        bgCover.classList.add('profilepic--bgcover')
+
+        //^ circle
+        const circleWrapper = document.createElement( 'div' );
+        circleWrapper.classList.add('wrapper--circle')
+
+        imgWrapper.appendChild(img);
+        imgWrapper.appendChild(bgCover);
+        imgWrapper.appendChild(circleWrapper);
+
+        //& creating what goes inside circleWrapper (the second layer of the profile picture (profilepic pp-top))
+        //^ pp-top
+        const imgTop = document.createElement( 'img' );
+        imgTop.setAttribute("src", picture)
+        imgTop.classList.add('profilepic')
+        imgTop.classList.add('pp--top')
+
+        circleWrapper.appendChild(imgTop);
+
+        //? creating the details section
+        const detailsSection = document.createElement('section')
+        detailsSection.classList.add('details')
+        article.appendChild(detailsSection)
+        //& creating what goes in the details section
+        const h3 = document.createElement('h3')
+        h3.textContent = city;
+        const p = document.createElement('p')
+        p.textContent = tagline;
+        const span = document.createElement('span')
+        span.textContent = price
+        //* adding detail elements
+        detailsSection.appendChild(h3)
+        detailsSection.appendChild(p)
+        detailsSection.appendChild(span)
+
+
         return (article);
     }
     return { name, picture, getUserCardDOM }
